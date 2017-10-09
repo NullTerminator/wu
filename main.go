63bbf6a5720b44cd6f6a696f027f3a2cdad3612b
Chris Marques <test.code.debug@gmail.com>
2017-10-09 00:30:22 -0400

    Use architectured components and get location from env var

--- main.go
+++ main.go
@@ -1,97 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-
-	"github.com/buger/goterm"
-)
-
-const (
-	API_ROOT = "http://api.wunderground.com/api/"
-)
-
-type (
-	ForecastConditions struct {
-		Forecast Forecast
-	}
-
-	Forecast struct {
-		Simpleforecast SimpleForecast
-	}
-
-	SimpleForecast struct {
-		Days []Forecastday `json:"forecastday"`
-	}
-
-	Forecastday struct {
-		Date           Forecastdate
-		High           Temperature
-		Low            Temperature
-		Conditions     string
-		ChanceOfPrecip int   `json:"pop"`
-		Rain           Depth `json:"qpf_allday"`
-		Snow           Depth `json:'snow_allday"`
-	}
-
-	Forecastdate struct {
-		Pretty          string
-		Day             int
-		Month           int
-		year            int
-		Monthname       string
-		Monthname_short string
-		Weekday         string
-		Weekday_short   string
-	}
-
-	Temperature struct {
-		F string `json:"Fahrenheit"`
-	}
-
-	Depth struct {
-		In float32
-	}
+	"wu/adapters"
+	"wu/infrastructure"
+	"wu/usecase"
 )
 
 func main() {
 	key := os.Getenv("WU_KEY")
-	url := fmt.Sprintf("%s%s/forecast10day/q/NH/Freedom.json", API_ROOT, key)
+	location := os.Getenv("WU_LOCATION")
 
-	res, err := http.Get(url)
-	CheckError(err)
-	if res.StatusCode != 200 {
-		fmt.Fprintf(os.Stderr, "Bad HTTP Status: %d\n", res.StatusCode)
-		os.Exit(1)
-	}
-	b, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	var conds ForecastConditions
-	jsonErr := json.Unmarshal(b, &conds)
-	CheckError(jsonErr)
+	handler := &infrastructure.HttpHandler{}
+	adapter := adapters.NewWundergroundAdapter(key, location, handler)
+	presenter := &adapters.BoxPresenter{}
 
-	goterm.Clear()
-	var boxes []*goterm.Box
-
-	for _, day := range conds.Forecast.Simpleforecast.Days {
-		fmt.Println(day)
-		box := goterm.NewBox(10|goterm.PCT, 20, 0)
-		fmt.Fprintf(box, "%s\n%s / %s\n%.2f in\n%d%%", day.Date.Weekday_short, day.High.F, day.Low.F, day.Rain.In, day.ChanceOfPrecip)
-		boxes = append(boxes, box)
-	}
-
-	for i, box := range boxes {
-		goterm.Print(goterm.MoveTo(box.String(), i*10|goterm.PCT, 90|goterm.PCT))
-	}
-	goterm.Flush()
-}
-
-func CheckError(err error) {
+	forecaster := usecase.NewForecastInteractor(adapter, presenter)
+	err := forecaster.Show()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error\n%v\n", err)
-		os.Exit(1)
+		panic(err)
 	}
 }
