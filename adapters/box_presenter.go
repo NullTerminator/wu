cf0de744d159b10e685863ae9c7f685584dc33c5
Chris Marques <test.code.debug@gmail.com>
2017-10-09 20:57:14 -0400

    Adjust box with based on number of days shown

--- adapters/box_presenter.go
+++ adapters/box_presenter.go
@@ -24,10 +24,12 @@ func NewBoxPresenter(logger Logger) BoxPresenter {
 func (presenter *boxPresenter) Print(days []*models.ForecastDay) error {
 	goterm.Clear()
 
-	presenter.logger.Debugf("BoxPresenter printing %d days", len(days))
+	dayCount := len(days)
+	presenter.logger.Debugf("BoxPresenter printing %d days", dayCount)
+	width := 100.0/dayCount - 1
 
 	for i, day := range days {
-		box := goterm.NewBox(10|goterm.PCT, 7, 0)
+		box := goterm.NewBox(width|goterm.PCT, 7, 0)
 		fmt.Fprintf(box, "%s\n%s / %s\n%s\n%.2f in\n%d%%",
 			day.WeekdayShort,
 			day.High, day.Low,
@@ -35,7 +37,7 @@ func (presenter *boxPresenter) Print(days []*models.ForecastDay) error {
 			day.Rain,
 			day.ChanceOfPrecip)
 
-		goterm.Print(goterm.MoveTo(box.String(), i*10|goterm.PCT, 1))
+		goterm.Print(goterm.MoveTo(box.String(), i*dayCount|goterm.PCT, 1))
 	}
 
 	goterm.Flush()
